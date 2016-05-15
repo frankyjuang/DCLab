f96b936dce8538b65b6d85e735e5eee8644d986d
JFJ <frankyjuang@gmail.com>
2016-05-15 00:10:01 +0000

    I2cinit sevenhex

--- Lab3/I2cInitialize.sv
+++ Lab3/I2cInitialize.sv
@@ -1,38 +1,99 @@
 module I2cInitialize (
     input i_clk,
+    input i_start,
     input i_rst,
-    output o_finished
+    output o_sclk,
+    output o_finished,
+    inout io_sdat
 );
+    
+    parameter IDLE = 1'b0;
+    parameter SEND = 1'b1;
 
-    parameter L_LINE_IN      = 24'b0011010_0_000_0000_0_1001_0111;
-    parameter R_LINE_IN      = 24'b0011010_0_000_0001_0_1001_0111;
-    parameter L_HEAD_OUT     = 24'b0011010_0_000_0010_0_0111_1001;
-    parameter R_HEAD_OUT     = 24'b0011010_0_000_0011_0_0111_1001;
-    parameter A_AUD_CONTROL  = 24'b0011010_0_000_0100_0_0001_0101;
-    parameter D_AUD_CONTROL  = 24'b0011010_0_000_0101_0_0000_0000;
-    parameter POWER_CONTROL  = 24'b0011010_0_000_0110_0_0000_0000;
-    parameter D_AUD_INTER    = 24'b0011010_0_000_0111_0_0100_0010;
-    parameter SAMPLE_CONTROL = 24'b0011010_0_000_1000_0_0001_1001;
-    parameter ACTIVE_CONTROL = 24'b0011010_0_000_1001_0_0000_0001;
+    // parameter [23:0] CONFIG_DATA [9:0] = {
+    parameter CONFIG_DATA [9:0] = {
+        24'b0011010_0_000_0000_0_1001_0111, //L_LINE_IN     
+        24'b0011010_0_000_0001_0_1001_0111, //R_LINE_IN     
+        24'b0011010_0_000_0010_0_0111_1001, //L_HEAD_OUT    
+        24'b0011010_0_000_0011_0_0111_1001, //R_HEAD_OUT    
+        24'b0011010_0_000_0100_0_0001_0101, //A_AUD_CONTROL 
+        24'b0011010_0_000_0101_0_0000_0000, //D_AUD_CONTROL 
+        24'b0011010_0_000_0110_0_0000_0000, //POWER_CONTROL 
+        24'b0011010_0_000_0111_0_0100_0010, //D_AUD_INTER   
+        24'b0011010_0_000_1000_0_0001_1001, //SAMPLE_CONTROL
+        24'b0011010_0_000_1001_0_0000_0001  //ACTIVE_CONTROL
+    };
 
-    logic [23:0] send_data_r, send_data_w;
-    logic [3:0]  counter_r, counter_w;
-    logic        finished_r, finished_w;
+    logic           state_r, state_w;
+    logic           sender_start_r, sender_start_w;
+    logic           finished_r, finished_w;
+    logic [23:0]    send_data_r, send_data_w;
+    logic [3:0]     counter_r, counter_w;
 
-    assign o_finished = finished_r;
+    logic           sender_finished, sclk;
+    wire            sdat;
+
+    assign o_finished   = finished_r;
+    assign o_sclk       = sclk;
+    assign io_sdat       = sdat;
+    
+    I2cSender #(.BYTE(3)) i2c_sender(
+        .i_clk(i_clk),
+        .i_rst(i_rst),
+        .i_dat(send_data_r),
+        .i_start(sender_start_r),
+        .o_finished(sender_finished),
+        .o_sclk(sclk),
+        .o_sdat(sdat)
+    );
 
     always_comb begin
-
+        state_r             = state_w;
+        sender_start_r      = sender_start_w;
+        send_data_r         = send_data_w;
+        finished_r          = finished_w;
+        counter_r           = counter_w;
+        
+        case (state_r)
+            IDLE:
+                begin
+                    if (i_start == 1'b1) begin
+                        state_w = SEND;
+                        send_data_w = CONFIG_DATA[counter_r];
+                        counter_w = counter_r + 1;
+                        sender_start_w = 1'b1;
+                    end
+                end
+            SEND:
+                begin
+                    //sender_start_w = 1'b0;
+                    if (counter_r < 10) begin
+                        if (sender_finished == 1'b1) begin
+                            send_data_w = CONFIG_DATA[counter_r];
+                            counter_w = counter_r + 1;
+                        end
+                    end
+                    if (counter_r == 9) begin
+                        state_w = IDLE;
+                        counter_w = 4'b0000;
+                        finished_w = 1'b1;
+                    end
+                end
+        endcase
     end
 
     always_ff @(posedge i_clk) begin
         if(i_rst) begin
-            counter_r <= 0;
-            send_data_r <= 0;
-            finished_r <= 0;
+            state_r             <= 0;
+            counter_r           <= 0;
+            send_data_r         <= 0;
+            finished_r          <= 0;
+            sender_start_r      <= 0;
         end else begin
-            send_data_r <= send_data_w;
-            counter_r <= counter_w;
-            finished_r <= finished_w;
+            state_r             <= state_w;
+            send_data_r         <= send_data_w;
+            counter_r           <= counter_w;
+            finished_r          <= finished_w;
+            sender_start_r      <= sender_start_w;
         end
     end
---
+++ Lab3/SevenHexAlphabet.sv
@@ -0,0 +1,61 @@
+module SevenHexAlphabet(
+	input [1:0] i_state,
+    output logic [6:0] o_seven_0,
+    output logic [6:0] o_seven_1,
+	output logic [6:0] o_seven_2,
+	output logic [6:0] o_seven_3
+);
+    /* The layout of seven segment display, 1: dark
+	 *    00
+	 *   5  1
+	 *    66
+	 *   4  2
+	 *    33
+	 */
+
+	parameter DA    = 7'b0001000;
+	parameter DC    = 7'b0000110;
+	parameter DD    = 7'b0100001;
+	parameter DE    = 7'b0000110;
+	parameter DI    = 7'b1111001;
+	parameter DL    = 7'b0111000;
+	parameter DP    = 7'b0001100;
+	parameter DR    = 7'b1001110;
+	parameter DS    = 7'b0010010;
+	parameter DU    = 7'b1000001;
+    parameter DY    = 7'b0010001;
+    parameter DCON  = 7'b1111111;
+
+	always_comb begin
+		case(i_state)
+			2'b00: 
+                begin 
+                    o_seven_3 = DI;
+                    o_seven_2 = DD;
+                    o_seven_1 = DL;
+                    o_seven_0 = DE;
+                end
+			2'b01: 
+                begin 
+                    o_seven_3 = DR;
+                    o_seven_2 = DE;
+                    o_seven_1 = DC;
+                    o_seven_0 = DCON;
+                end
+			2'b10: 
+                begin 
+                    o_seven_3 = DP;
+                    o_seven_2 = DL;
+                    o_seven_1 = DA;
+                    o_seven_0 = DY;
+                end
+			2'b11: 
+                begin 
+                    o_seven_3 = DP;
+                    o_seven_2 = DA;
+                    o_seven_1 = DU;
+                    o_seven_0 = DS;
+                end
+        endcase
+	end
+endmodule
